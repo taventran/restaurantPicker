934a6cd20686bc9e7d03933895dfa52678ff0a91
Taven Tran <taventran@gmail.com>
2023-07-03 00:20:47 -0500

    Cleaned up stars icon and added icons to select price range. Also added a slider to determind distance

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,48 +1,126 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faDollar } from '@fortawesome/free-solid-svg-icons';
 import './home.css';
 
 function Home() {
 
-    const [ highlighted, setHighlighted ] = useState(-1);
-    const [ clicked, setClicked ] = useState(-1)
+    const [ rateHighlighted, setRateHighlighted ] = useState(-1);
+    const [ ratingClicked, setRatingClicked ] = useState(-1);
+    const [ showEditRating, setShowEditRating ] = useState(true);
     
     const highlightRate = high => evt => {
-        setHighlighted(high);
+        setRateHighlighted(high);
     }
 
     const rateClicked = rate => evt => {
-        setClicked(rate + 1);
+        setRatingClicked(rate + 1);
+        enableEditRating();
     }
 
-    
+    function enableEditRating() {
+        if (showEditRating === false) {
+            setShowEditRating(true)
+        }
+        else {
+            setShowEditRating(false)
+        }
+    }
+
+    const [ dollarHighlighted, setDollarHighlighted ] = useState(-1);
+    const [ dollarSignClicked, setDollarClicked ] = useState(-1);
+    const [ showEditDollar, setShowEditDollar ] = useState(true);
+
+    const highlightDollar = high => evt => {
+        setDollarHighlighted(high);
+    }
+
+    const dollarClicked = rate => evt => {
+        setDollarClicked(rate + 1);
+        enableEditDollarSign();
+    }
+
+    function enableEditDollarSign() {
+        if (showEditDollar === false) {
+            setShowEditDollar(true)
+        }
+        else {
+            setShowEditDollar(false)
+        }
+    }
+
+    const [sliderValue, setSliderValue] = useState(50);
+
+    const handleSliderChange = (event) => {
+      const value = parseInt(event.target.value);
+      setSliderValue(value);
+    };
 
     return (
         <React.Fragment>
             <div className="Home">
                 <div className="Rating">
                     <h5>Rating:</h5>
-                    <FontAwesomeIcon icon={faStar} className={clicked > 0 ? 'orange':''}/>
-                    <FontAwesomeIcon icon={faStar} className={clicked > 1 ? 'orange':''}/>
-                    <FontAwesomeIcon icon={faStar} className={clicked > 2 ? 'orange':''}/>
-                    <FontAwesomeIcon icon={faStar} className={clicked > 3 ? 'orange':''}/>
-                <FontAwesomeIcon icon={faStar} className={clicked > 4 ? 'orange':''}/>
-                    <div className="rate-container">
-                    {
-                        [...Array(5)].map( (e, i)=> {
-                            return <FontAwesomeIcon key={i} icon={faStar} className={highlighted > i - 1 ? 'purple':''}
-                            onMouseEnter={highlightRate(i)}
-                            onMouseLeave={highlightRate(-1)}
-                            onClick={rateClicked(i)}
-                        />
-                        }) 
+                    {showEditRating === true &&
+                        /* Editing restaurant rating criteria */
+                        <div className="rate-container">
+                        {
+                            [...Array(5)].map( (e, i)=> {
+                                return <FontAwesomeIcon key={i} icon={faStar} className={rateHighlighted > i - 1 ? 'orange':''}
+                                onMouseEnter={highlightRate(i)}
+                                onMouseLeave={highlightRate(-1)}
+                                onClick={rateClicked(i)}
+                            />
+                            }) 
+                        }
+                        </div>
+                    }
+                    {showEditRating === false &&
+                        /* Show minimum rating requirement */
+                        <div>
+                            <FontAwesomeIcon icon={faStar} className={ratingClicked > 0 ? 'orange':''}/>
+                            <FontAwesomeIcon icon={faStar} className={ratingClicked > 1 ? 'orange':''}/>
+                            <FontAwesomeIcon icon={faStar} className={ratingClicked > 2 ? 'orange':''}/>
+                            <FontAwesomeIcon icon={faStar} className={ratingClicked > 3 ? 'orange':''}/>
+                            <FontAwesomeIcon icon={faStar} className={ratingClicked > 4 ? 'orange':''}/>
+                            <FontAwesomeIcon icon={faEdit} className="edit-icon" onClick={enableEditRating}/>
+                        </div>
                     }
                 </div>
+                <div className="Price">
+                    <h5>Price:</h5>
+                    {showEditDollar === true &&
+                        /* Editing max dollar range criteria */
+                    <div className="dollar-container">
+                        {
+                            [...Array(4)].map( (e, i)=> {
+                                return <FontAwesomeIcon key={i} icon={faDollar} className={dollarHighlighted > i - 1 ? 'green':''}
+                                onMouseEnter={highlightDollar(i)}
+                                onMouseLeave={highlightDollar(-1)}
+                                onClick={dollarClicked(i)}
+                            />
+                            }) 
+                        }
+                    </div>
+                    }
+                    {showEditDollar === false &&
+                        /* Show max dollar range requirement */
+                        <div>
+                            <FontAwesomeIcon icon={faDollar} className={dollarSignClicked > 0 ? 'green':''}/>
+                            <FontAwesomeIcon icon={faDollar} className={dollarSignClicked > 1 ? 'green':''}/>
+                            <FontAwesomeIcon icon={faDollar} className={dollarSignClicked > 2 ? 'green':''}/>
+                            <FontAwesomeIcon icon={faDollar} className={dollarSignClicked > 3 ? 'green':''}/>
+                            <FontAwesomeIcon icon={faEdit} className="edit-icon" onClick={enableEditDollarSign}/>
+                        </div>
+                    }
                 </div>
-                <h5>Price</h5>
-                <h5>Distance</h5>
-                <h5>Type</h5>
+                <h5>Distance: {sliderValue} miles</h5>
+                {/* Setting the max distance possible */}
+                <input type="range" class="form-range" min="0" max="100" step="5" 
+                    id="customRange1"  onChange={handleSliderChange}/>
+                <h5>Type:</h5>
              </div>
              <div className="d-flex justify-content-center align-items-center">
                 <button className="btn btn-primary btn-lg">generate random restaurant</button>
