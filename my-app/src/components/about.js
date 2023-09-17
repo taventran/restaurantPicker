c4a4cd2467ffa6295f9a1995111345880b6a67c5
Taven Tran <taventran@gmail.com>
2023-09-16 20:38:48 -0500

    Made about page more cleaer

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
       <React.Fragment>
         <div className="info">
           <h1>How it Works</h1>
-          <p> This simple website is for randomly generating a yelp restaurant using yelp's fusion API</p>
+          <p> This simple website is for randomly generating a restaurant using yelp's fusion API</p>
           <p>1. Add minimum star requirement up to 5 starts.</p>
           <p>2. Choose maxmium price of restaurant up to 4 dollar signs.</p>
           <p>3. Choose maximum radius away from your location.</p>
